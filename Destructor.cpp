3a4b59732d1c7281cc7104da982a14765a1ba2f6
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-05-17 15:54:14 +0700

    membuat fungsi untuk memanggil cetakdata serta fungsi menghapus array

--- Destructor.cpp
+++ Destructor.cpp
@@ -19,4 +19,10 @@ angka::angka(int i) { // Constructor
     isiData();
 }
 
+angka::~angka() { // Destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
 
