20db9976dbe94802f7a0f2795482acb8b409c74f
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-05-17 15:32:26 +0700

    membuat metod setJudul dan getJudul pada class buku

--- Constructor.cpp
+++ Constructor.cpp
@@ -5,6 +5,15 @@ class buku
 {
     string judul;
 
-
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // chain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
 } 
 
